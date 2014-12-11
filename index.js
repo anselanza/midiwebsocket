f9f9b592519ff3630effef65964127f5b0a8f91b
Stephen Buchanan <stephen@mannmademedia.com>
2014-12-11 15:56:41 +0200

    init commit with angular-fullstack generation

--- index.js
+++
@@ -1,9 +0,0 @@
-var connect = require('./node_modules/midi.io/node_modules/connect'),
-    socketio = require('./node_modules/midi.io/node_modules/socket.io'),
-    midi = require('midi.io');
-
-var server = connect.createServer(),
-    io = socketio.listen(server);
-
-server.use(midi(io));
-server.listen(9000);
